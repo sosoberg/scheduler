d8c862f1b7c4e9a25e0e0ff35067d86be3554b5f
Sam Oberg <samueloberg@hotmail.com>
2021-03-02 19:27:39 -0800

    more styling and script work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
 //calling moment to get todays date
+var nine = document.querySelector('#nine');
+
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
+
+var hour = moment().format('hh:mm:ss');
+
+console.log(hour);
+
+var timeCheck = '';
+/*
+if (hour > 10) {
+    nine.removeClass('present');
+    nine.addClass('future');
+} else if (hour < 9) {
+    nine.removeClass('present');
+    nine.addClass('past')
+} else {
+    return;
+}
+*/
